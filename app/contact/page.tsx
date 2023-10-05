246e3a263560b506ceae55a678614f5cc3a6e29f
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-10-05 21:01:01 +0600

    Made the contact section fully responsive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,24 +7,21 @@ export default function page() {
   return (
     <>
       <div className="min-h-[100vh]">
-        <div className="pt-40 container m-auto flex justify-between  items-start h-[100%]">
+        <div className="pt-20 lg:pt-40 xl:pt-40 2xl:pt-40 container pb-5 m-auto flex justify-between  items-start h-[100%] px-5">
           <div>
             <h1 className="text-4xl mb-5 font-bold">Contact Us</h1>
-            <p className="max-w-[700px] leading-[200%] text-justify">
+            <p className="max-w-[700px] leading-[200%] text-left lg:text-justify xl:text-justify 2xl:text-justify">
               We'd love to hear from you! We're not really all that evil, and we
               love discussing potential projects, intriguing ideas, and new
-              opportunities. Complete the form below or drop us a line at
-              <span className="ml-2 underline">
-                codeverseexplorer@gmail.com.
-              </span>
+              opportunities. Complete the form below
             </p>
-            <form className="mt-5 flex flex-col w-[100%]">
+            <form className="mt-5 flex flex-col ">
               <textarea
-                className="border-2 p-2 mb-5"
+                className="border-2 p-2 mb-5 w-[100%]"
                 name="message"
                 id="message"
-                cols="100"
-                rows="10"
+                cols={100}
+                rows={10}
                 placeholder="Enter your message"
               ></textarea>
               <div className="flex justify-between">
@@ -32,13 +29,13 @@ export default function page() {
                   type="text"
                   id="name"
                   placeholder="Enter your name"
-                  className="border-2 p-2 w-[35%]"
+                  className="border-2 p-2 mr-5 w-[100%]"
                 />
                 <input
                   type="email"
                   id="email"
                   placeholder="Enter your email"
-                  className="border-2 p-2 w-[60%]"
+                  className="border-2 p-2 w-[100%]"
                 />
               </div>
               <button
@@ -49,7 +46,7 @@ export default function page() {
               </button>
             </form>
           </div>
-          <div>
+          <div className="hidden lg:block xl:block 2xl:block">
             <Image src="/contact.svg" width={500} height={500} alt="fuck" />
           </div>
         </div>
